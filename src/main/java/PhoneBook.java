a357beb9ad517245ff7f3eed34ddcb09278c3d9a
Maksim <madmaxim22@gmail.com>
2023-02-25 12:35:30 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,14 @@
+import java.util.Map;
+
 public class PhoneBook {
+
+    Map<String, String> mapPhone;
+
+    public Map<String, String> getPhoneBook() {
+        return mapPhone;
+    }
+
+    int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PhoneBookTest {
+
+    PhoneBook phoneBook;
+    @BeforeEach
+    public void newPhoneBook() {
+        phoneBook = new PhoneBook();
+    }
+    @Test
+    public void testAdd() {
+        phoneBook.add("Максим", "89267095433");
+        assertTrue(phoneBook.mapPhone.containsKey("имя"));
+        assertTrue(phoneBook.mapPhone.containsValue("номер"));
+    }
+
+    @Test
+    public void testAddReturnSize() {
+        int size = phoneBook.add("Максим", "89267095433");
+        assertEquals(0, size);
+    }
+
+    @Test
+    public void testAddNotDuplication() {
+        phoneBook.add("Максим", "89267095433");
+        phoneBook.add("Максим", "89992341565");
+        assertTrue(phoneBook.mapPhone.containsValue("89267095433"));
+    }
 }
